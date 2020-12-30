10b52a4cdcb2965be6e4ec12c96e882630843678
Jennifer Morales <jennifer.morales1201@gmail.com>
2020-12-30 14:06:12 -0500

    Installed id generator, added some styling, add button is functional

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function ToDoList() {
   let [text, setText] = useState('');
@@ -10,11 +11,25 @@ function ToDoList() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setToDo(todos.push({ text }));
-    text = '';
+    if (text === '') {
+      return;
+    }
+    let newTodo = { text: text, id: uuidv4() };
+    setToDo((todos = [newTodo, ...todos]));
+    setText((text = ''));
   };
 
-  console.log('this is todos', todos);
+  const deleteToDo = (ToDoId) => {
+    let toDoArray = todos;
+
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i].id === ToDoId) {
+        toDoArray.splice(i, 1);
+        return toDoArray;
+      }
+    }
+    setToDo(todos = toDoArray);
+  };
 
   return (
     <>
@@ -36,10 +51,15 @@ function ToDoList() {
           </button>
         </div>
       </form>
-      {/* <div className="notification is-primary">
-        <button className="delete"></button>
-        {text}
-      </div> */}
+      {todos.map((todo) => (
+        <div className="notification is-primary" key={todo.id}>
+          <button
+            className="delete"
+            onClick={() => deleteToDo(todo.id)}
+          ></button>
+          {todo.text}
+        </div>
+      ))}
     </>
   );
 }
